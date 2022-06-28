244297976f3a7aad8034c4785eadafea43ab3002
Lucia Langaney <langaneylucia03@gmail.com>
2022-06-28 08:36:22 -0700

    Initial Commit

---
+++ Green Life/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  Green Life
+//
+//  Created by MAC on 6/27/22.
+//
+
+import UIKit
+
+class toDO {
+    var name = ""
+    var important = false
+}
---
+++ Green Life/ToDoTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  cocoClassTableViewController.swift
+//  Green Life
+//
+//  Created by MAC on 6/27/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toDos = createToDos()
+        
+    }
+    
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return toDos.count
+    }
+
+    
+}
